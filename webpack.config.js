983fc22b93fe67d13edad9322a3be2dafe4008b5
하지현 <kkim.haji@gmail.com>
2022-06-16 15:34:07 +0900

    gitignore 설정, webpack 기본 설정

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const distPath = path.join(__dirname, 'dist');
+
+module.exports = {
+
+    mode: 'development',
+
+    entry: './src/index.js',
+    devServer:{
+        static:{
+            directory: distPath,
+        },
+        compress: true,
+        port:9000,
+    },
+    output: {
+        path: distPath,
+        filename: 'main.js',
+    },
+    plugins:[ new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+
+}
